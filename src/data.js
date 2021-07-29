5746f00760c121ee13a7c55c9e5517d1eb4173f3
Ishansh16gupta <84246007+Ishansh16gupta@users.noreply.github.com>
2021-07-29 15:52:03 +0530

    Oxile commit files

---
+++ src/data.js
@@ -0,0 +1,77 @@
+const data = {
+  products:[
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "images/product-1.jpg",
+      price: "$120",
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "High quality product",
+    },
+
+    {
+      _id: "2",
+      name: "Adidas Slim Shirt",
+      category: "Shirts",
+      image: "images/product-2.jpg",
+      price: "$110",
+      brand: "Adidas",
+      rating: 4.3,
+      numReviews: 10,
+      description: "High quality product",
+    },
+
+    {
+      _id: "3",
+      name: "Puma Slim Shirt",
+      category: "Shirts",
+      image: "images/product-3.jpg",
+      price: "$90",
+      brand: "Puma",
+      rating: 4.0,
+      numReviews: 10,
+      description: "High quality product",
+    },
+
+    {
+      _id: "4",
+      name: "Relaxo Slim Shirt",
+      category: "Shirts",
+      image: "images/product-4.jpg",
+      price: "$100",
+      brand: "Relaxo",
+      rating: 3.9,
+      numReviews: 10,
+      description: "High quality product",
+    },
+
+    {
+      _id: "5",
+      name: "Gucci Slim Shirt",
+      category: "Shirts",
+      image: "images/product-5.jpg",
+      price: "$80",
+      brand: "Gucci",
+      rating: 4.4,
+      numReviews: 10,
+      description: "High quality product",
+    },
+
+    {
+      _id: "6",
+      name: "Zara Slim Shirt",
+      category: "Shirts",
+      image: "images/product-6.jpg",
+      price: "$120",
+      brand: "Zara",
+      rating: 3.0,
+      numReviews: 10,
+      description: "High quality product",
+    },
+  ],
+};
+
+export default data;
